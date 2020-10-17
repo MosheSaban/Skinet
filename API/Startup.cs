0f6bd97e19fdf7891a91ed388d20af55ce58d7cf
Moshe Saban <moshir321@gmail.com>
2020-10-17 19:01:58 +0300

    End of section 13 (API Basket)

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,9 +37,9 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
-            services.AddSingleton<ConnectionMultiplexer>(c => {
-                var configuration = ConfigurationOptions.Parse(_configuration.GetConnectionString("Redis"),
-                true);
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(_configuration.GetConnectionString("Redis"), true);
                 return ConnectionMultiplexer.Connect(configuration);
             });
             services.AddApplicationServices();
